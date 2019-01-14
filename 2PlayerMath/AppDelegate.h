efe283968dcee841a716a4b6c103f279255b82ae
Penjat <theSpencerSymington@Gmail.com>
2019-01-14 14:13:28 -0800

    Initial Commit

---
+++ 2PlayerMath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2PlayerMath
+//
+//  Created by Spencer Symington on 2019-01-14.
+//  Copyright © 2019 Spencer Symington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
